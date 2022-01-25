799a86287632c9f30c8126b56d0c4f5ffa183b58
Zurk1245 <marianito01@live.com>
2022-01-25 18:09:04 -0300

    Entregable API RESTful terminado

--- api_restful/index.js
+++ api_restful/index.js
@@ -1,42 +1,85 @@
 const express = require('express');
 const app = express();
+const productAPI = require('./api/productos');
+const { ProductManagement } = productAPI;
 const PORT = 8080;
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+const productos = express.Router();
+app.use('/api/productos', productos);
+productos.use(express.json());
+productos.use(express.urlencoded({extended: true}));
+
+const contenedor =  new ProductManagement();
+contenedor.products = [
+    {
+		title: "Manzana",
+		price: "20",
+		thumbnail: "https://cdn4.iconfinder.com/data/icons/fruits-79/48/04-apple-256.png",
+		id: 1
+	},
+	{
+		title: "Naranja",
+		price: "10",
+		thumbnail: "https://cdn1.iconfinder.com/data/icons/food-3-11/128/food_Orange-Citrus-Juice-Fruit-2-256.png",
+		id: 2
+	}
+]
+
+productos
+    .route('/')
+    .get((req, res) => {
+        res.send(contenedor.products);
+    })
+    .post((req, res) => {
+        const newProduct = {
+            title: req.body.title,
+            price: req.body.price,
+            thumbnail: req.body.thumbnail
+        }
+        contenedor.save(newProduct);
+        res.send(newProduct);
+    });
+
+productos
+    .route('/:id')
+    .get((req, res) => {
+        //console.log(req.params.id);
+        const id = req.params.id;
+        const desiredProduct = contenedor.products.find(product => product.id == id);
+        if (desiredProduct) {
+            res.send(desiredProduct);
+        } else {
+            res.send({ error : 'producto no encontrado' });
+        }
+    })
+    .put((req, res) => {
+        const id = req.params.id;
+        const productToUpdate = contenedor.products.find(product => product.id == id);
+        if (productToUpdate) {
+            const newTitle = req.body.title;
+            const newPrice = req.body.price;
+            const newThumbnail = req.body.thumbnail;
+            contenedor.updateById(newTitle, newPrice, newThumbnail, id);    
+            res.send(`Producto ${id} actualizado`);
+        } else {
+            res.send({ error : 'producto a actualizar no encontrado' });
+        }
+    })
+    .delete((req, res) => {
+        const id = req.params.id;
+        const productToDelete = contenedor.products.find(product => product.id == id);
+        if (productToDelete) {
+            contenedor.deleteById(id);
+            res.send(`Producto ${id} eliminado`);
+        } else {
+            res.send({ error : 'producto a eliminar no encontrado' });
+        }
+    });
+
+productos.use('/static', express.static('public'));
 
 
-
-app.get('/', (req, res) => {
-    res.send({message: 'Hello world!'});
-});
-
-app.get('/api/productos', (req, res) => {
-    res.send({});
-});
-
-app.get('/api/productos/:id', (req, res) => {
-    console.log('hola');
-    //console.log(req);
-    res.send({Id: req.params.id});
-});
-
-app.post('/api/productos', (req, res) => {
-    res.send({});
-});
-
-app.put('/api/productos/:id', (req, res) => {
-    res.send({});
-});
-
-app.delete('/api/productos/:id', (req, res) => {
-    res.send({})
-});
-
-
-
-
-
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server live at port`, PORT);
 });
+server.on("error", (error) => console.log(`error en el servidor ${error}`));
