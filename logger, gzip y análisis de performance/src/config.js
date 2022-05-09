65f5720765700910d57aacb79c9f91cd1eef3499
Zurk1245 <marianito01@live.com>
2022-05-09 15:08:57 -0300

    v0.0.1

--- logger, gzip y análisis de performance/src/config.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-
-module.exports = {
-    mongodbRemote: {
-        client: "mongodb",
-        cnxStr: process.env.CONNECTION_STRING,
-    }
-}
--- logger, gzip y análisis de performance/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { isLogged } = require("../middleware-functions");
-const index = express.Router();
-
-index.get("/", isLogged, (req, res) => {
-    res.redirect("/home");
-});
-
-module.exports = index;
